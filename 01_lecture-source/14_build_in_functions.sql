d99f1513e951656e1fcf1dd3c87390f24ffc1c03
yoonjae <realyoon77@gmail.com>
2024-03-04 18:01:15 +0900

    [2024-03-04] 학습내용 정리

--- 01_lecture-source/14_build_in_functions.sql
+++ 01_lecture-source/14_build_in_functions.sql
@@ -69,4 +69,77 @@ SELECT SUBSTRING_INDEX('hong.test@gmail.com','.',-2);
 -- abs(숫자) : 절댓값 반환
 SELECT ABS(-1234);
 
+-- ceiling (숫자) : 올림값 반환 (올림)
+-- floor(숫자) : 버림값 반환	(내림)
+-- round(숫자) : 반올림값 반환 (반올림)
+SELECT CEILING(1234.56),FLOOR(1234.56),ROUND(1234.56);
 
+-- mod(숫자1,숫자2) or 숫자1 % 숫자2 or 숫자1 mod 숫자2
+-- 숫자 1을 숫자 2로 나눈 나머지 추출
+SELECT MOD(75,10),75%10,75 MOD 10;
+
+-- pow(숫자1, 숫자2) : 거듭제곱값
+-- sqrt(숫자) : 제곱근을 추출
+
+SELECT POW(2,4), SQRT(16);
+
+-- rand() : 0 이상 1 미만의 실수를 구한다.
+-- m <= 임의의 정수 < n 을 구하고 싶다면
+-- floor( (rand() *(n - m) + m )
+-- 1 부터 10 까지 난수 : floor(rand() * (11 - 1) + 1)
+
+SELECT RAND(), FLOOR(RAND() * 10 + 1);
+
+ -- sign(숫자)
+ -- 양수면 1, 0이면 0, 음수면 -1 을 반환
+SELECT SIGN(10.1),SIGN(0),SIGN(-10);
+
+-- truncate(숫자, 정수);
+-- 소수점을 기준으로 정수 위치까지 구하고 나머지 버림
+SELECT TRUNCATE(12345.1235,-2);
+
+-- 날짜 및 시간 관련 함수
+-- adddate(날짜, 차이) : 날짜를 기준으로 차이를 더함
+-- subdate(날짜, 차이) : 날짜를 기준으로 날짜를 뺌
+SELECT ADDDATE('2024-03-04',INTERVAL 30 DAY);
+SELECT ADDDATE('2024-03-04',INTERVAL 6 MONTH);
+SELECT ADDDATE('2024-03-04',INTERVAL 6 year);
+
+SELECT SUBDATE('2024-03-04',INTERVAL 30 DAY);
+
+-- addtime(날짜/시간,시간)
+-- subtime(날짜/시간,시간)
+SELECT ADDTIME('2024-03-04 17:43:00','0:30');
+SELECT SUBTIME('2024-03-04 17:43:00','0:30');
+
+-- curdate : 현재 연-월-일
+-- curtime : 현재 시:분:초
+-- now() or sysdate() : 현재 연-월-일 시:분:초
+SELECT CURDATE(), CURTIME(), NOW(), SYSDATE();
+
+-- year(날짜), month(날짜), day(날짜)
+-- 연 월 일 시 분 초 밀리초
+SELECT YEAR(CURDATE()),MONTH(CURDATE()),MICROSECOND(NOW());
+
+-- datediff (날짜 1, 날짜 2)
+-- timediff( 날짜 1 or 시간 1, 날짜2 또는 시간 2 )
+-- datediff : 날짜 1 - 날짜 2의 일수를 반환
+-- timediff : 시간 1 - 시간 2의 결과
+SELECT DATEDIFF('2024-03-07',NOW()),TIMEDIFF('17:05:11','13:00:10');
+
+-- dayofweek(날짜) : 요일 반환 (일요일이 1)
+-- monthname : 해당 달의 이름 반환
+-- dayofyear : 해당 년도에서 몇 일이 흘렀는 지
+
+SELECT DAYOFWEEK(CURDATE()),MONTHNAME(CURDATE()),DAYOFYEAR(CURDATE());
+
+-- last_day(날짜)
+-- 해당 날짜의 달에서 마지막 날의 날짜를 구한다.
+
+SELECT LAST_DAY(CURDATE());
+
+-- quarter(날짜) : 해당 날짜의 분기를 구한다.
+SELECT QUARTER(CURDATE());
+
+-- time_to_sec(시간) : 시간을 초 단위로 구함
+SELECT TIME_TO_SEC('1:1:1');
